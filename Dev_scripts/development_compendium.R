f833d4de6e5f5c29fc7cf30503104a3deab0d97d
Lola Gilbert <lola.gilbert@lilo.org>
2021-12-08 17:30:48 +0100

    Create development script
    keep track of what has been done!

---
+++ Dev_scripts/development_compendium.R
@@ -0,0 +1,32 @@
+################################################################################
+# Lola Gilbert lola.gilbert@univ-lr.fr
+# 2021/12/08
+#
+# development_compendium.R
+#
+# Script with basic command to create and develop compendium
+################################################################################
+
+############################### TO DO ONLY ONCE
+# create folder with development script
+dir.create("Dev_scripts")
+
+# initiate renv
+renv::init()
+
+# load package to create compendium
+renv::install("benmarwick/rrtools")
+
+# create compendium 
+rrtools::use_compendium(path = "../Excretion.global.compendium/",
+                        open = FALSE)
+
+############################### renv update
+# check renv status
+renv::status()
+
+# update renv lock file
+renv::snapshot()
+
+# restore or activate renv 
+renv::activate()
